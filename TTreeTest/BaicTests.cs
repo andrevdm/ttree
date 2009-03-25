1a7157bc9378a5ac7cb55348864a705f5a2ca568
Andre Van Der Merwe <dart@pobox.com>
2009-03-25 22:40:50 +0200

    Initial project creation

---
+++ TTreeTest/BaicTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TTreeTest
+{
+	[TestClass]
+	public class BaicTests
+	{
+		[TestMethod]
+		public void TestTest()
+		{
+		}
+		
+		public TestContext TestContext { get; set; }
+	}
+}
