d744ea3018b28d0273908bdbc597fa5d21a9a457
Andre Van Der Merwe <dart@pobox.com>
2009-03-26 16:21:52 +0200

    Add to non-full root node working
    Using Array.BinarySearch and Array.Copy

--- TTree/Tree.cs
+++ TTree/Tree.cs
@@ -5,7 +5,77 @@ using System.Text;
 
 namespace TTree
 {
-	public class Tree
+	public class Tree<T> //visitor, IEnumerable etc
+		where T : IComparable
 	{
+		protected readonly T[] m_data;
+		protected readonly int m_minimum;
+
+		public Tree( int minimum, int maximum )
+		{
+			#region param checks
+			if( minimum < 1 )
+				throw new ArgumentOutOfRangeException( "order", "Expecting an order of at least 1." );
+
+			if( maximum < minimum )
+				throw new ArgumentOutOfRangeException( "maximum", "Maximum value must be greater than the minimum. " );
+			#endregion
+
+			m_data = new T[ maximum ];
+			Count = 0;
+			m_minimum = minimum;
+		}
+
+		public void Insert( T item )
+		{
+			//Is there space in this node?
+			if( Count < m_minimum )
+			{
+				int closest = Array.BinarySearch<T>( m_data, 0, Count, item );
+
+				if( closest < 0 )
+				{
+					closest = ~closest;
+
+					if( closest > Count )
+						closest = Count;
+				}
+
+				Array.Copy( m_data, closest, m_data, closest + 1, Count - closest );
+				m_data[ closest ] = item;
+				Count++;
+			}
+			else
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Delete( T item )
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Search( T item )
+		{
+			//Array.BinarySearch<T>( m_data, 
+
+			throw new NotImplementedException();
+		}
+
+		public T Search( Func<T, bool> match )
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyArray( T[] destinationArray, int index )
+		{
+			m_data.CopyTo( destinationArray, index );
+		}
+
+		public int Count { get; protected set; }
+		public Tree<T> Parent { get; set; }
+		public Tree<T> Left { get; set; }
+		public Tree<T> Right { get; set; }
 	}
 }
--- TTreeTest/BaicTests.cs
+++ TTreeTest/BaicTests.cs
@@ -4,14 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using TTree;
+
 namespace TTreeTest
 {
 	[TestClass]
 	public class BaicTests
 	{
 		[TestMethod]
-		public void TestTest()
+		public void InsertIntoRootWithSpace()
 		{
+			var tree = new Tree<int>( 10, 13 );
+
+			tree.Insert( 10 );
+			tree.Insert( 5 );
+			tree.Insert( 15 );
+			tree.Insert( 7 );
+			tree.Insert( 3 );
+
+			var data = new int[ 13 ];
+			tree.CopyArray( data, 0 );
+
+			Assert.AreEqual( 5, tree.Count, "Invalid count" );
+			Assert.AreEqual( 3, data[ 0 ], "Invalid value at index 0" );
+			Assert.AreEqual( 5, data[ 1 ], "Invalid value at index 1" );
+			Assert.AreEqual( 7, data[ 2 ], "Invalid value at index 2" );
+			Assert.AreEqual( 10, data[ 3 ], "Invalid value at index 3" );
+			Assert.AreEqual( 15, data[ 4 ], "Invalid value at index 4" );
 		}
 		
 		public TestContext TestContext { get; set; }
